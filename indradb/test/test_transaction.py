2cff4b23becae698e5c34c6636e051b2cf694aa5
Yusuf Simonson <simonson@gmail.com>
2018-01-15 19:55:22 -0500

    Remove `EdgeQuery.Edge` and `VertexQuery.Vertex`, as they are no longer supported`

--- indradb/test/test_transaction.py
+++ indradb/test/test_transaction.py
@@ -15,19 +15,19 @@ class TransactionTestCase(unittest.TestCase):
 
     def test_get_vertices(self):
         uuid = self.single(Transaction().create_vertex("foo"))
-        results = self.single(Transaction().get_vertices(VertexQuery.vertex(uuid)))
+        results = self.single(Transaction().get_vertices(VertexQuery.vertices([uuid])))
         self.assertEqual(len(results), 1)
         self.assertEqual(results[0].id, uuid)
 
     def test_delete_vertices(self):
         uuid = self.single(Transaction().create_vertex("foo"))
-        [_, results] = self.client.transaction(Transaction().delete_vertices(VertexQuery.vertex(uuid)).get_vertices(VertexQuery.vertex(uuid)))
+        [_, results] = self.client.transaction(Transaction().delete_vertices(VertexQuery.vertices([uuid])).get_vertices(VertexQuery.vertices([uuid])))
         self.assertEqual(len(results), 0)
 
     def test_get_edges(self):
         [outbound_id, inbound_id] = self.client.transaction(Transaction().create_vertex("foo").create_vertex("foo"))
         key = EdgeKey(outbound_id, "bar", inbound_id)
-        [_, results] = self.client.transaction(Transaction().create_edge(key, 0.5).get_edges(EdgeQuery.edge(key)))
+        [_, results] = self.client.transaction(Transaction().create_edge(key, 0.5).get_edges(EdgeQuery.edges([key])))
         self.assertEqual(len(results), 1)
         self.assertEqual(results[0].key.outbound_id, outbound_id)
         self.assertEqual(results[0].key.type, "bar")
@@ -37,13 +37,13 @@ class TransactionTestCase(unittest.TestCase):
     def test_get_edge_count(self):
         [outbound_id, inbound_id] = self.client.transaction(Transaction().create_vertex("foo").create_vertex("foo"))
         key = EdgeKey(outbound_id, "bar", inbound_id)
-        [_, count] = self.client.transaction(Transaction().create_edge(key, 0.5).get_edge_count(EdgeQuery.edge(key)))
+        [_, count] = self.client.transaction(Transaction().create_edge(key, 0.5).get_edge_count(EdgeQuery.edges([key])))
         self.assertEqual(count, 1)
 
     def test_delete_edges(self):
         [outbound_id, inbound_id] = self.client.transaction(Transaction().create_vertex("foo").create_vertex("foo"))
         key = EdgeKey(outbound_id, "bar", inbound_id)
-        [_, _, count] = self.client.transaction(Transaction().create_edge(key, 0.5).delete_edges(EdgeQuery.edge(key)).get_edge_count(EdgeQuery.edge(key)))
+        [_, _, count] = self.client.transaction(Transaction().create_edge(key, 0.5).delete_edges(EdgeQuery.edges([key])).get_edge_count(EdgeQuery.edges([key])))
         self.assertEqual(count, 0)
 
     def test_run_script(self):
