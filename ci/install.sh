84cdfd2c03729392a7033ae8f036868c4312ac00
Yusuf Simonson <simonson@gmail.com>
2018-10-12 18:26:43 -0400

    Install capnp

---
+++ ci/install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -ex
+
+# Use sed to replace the SSH URL with the public URL, then initialize
+# submodules. This is done because TravisCI can't pull from the SSH
+# URI. Via https://stackoverflow.com/questions/15674064/how-to-fix-a-permission-denied-publickey-error-for-a-git-submodule-update-in-t
+sed -i 's/git@github.com:/https:\/\/github.com\//' .gitmodules
+git submodule update --init --recursive
+
+
+sudo apt-get -qq update
+sudo apt-get install -y python3
+
+if [ $TOXENV = pypy ]; then
+    sudo apt-get install -y pypy;
+fi
+
+virtualenv -p python3 venv && source venv/bin/activate && pip install tox nose requests
+
+curl -O https://capnproto.org/capnproto-c++-0.6.1.tar.gz
+tar zxf capnproto-c++-0.6.1.tar.gz
+cd capnproto-c++-0.6.1
+./configure
+make -j6 check
+sudo make install
+source ~/.cargo/env || true
