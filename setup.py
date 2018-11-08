90de8bc1ea292b53565323e2d5f2e160a950e378
Yusuf Simonson <simonson@gmail.com>
2018-11-08 11:47:11 -0500

    v0.6.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = "indradb",
-    version = "0.5.0",
+    version = "0.6.0",
     author = "Yusuf Simonson",
     package_data={"": ["indradb.capnp"]},
 
